e1a151c1b12a3f35921842ee22fae768d27e3f36
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-09-12 14:59:48 -0400

    Edited the  userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,23 +1,28 @@
 const router = require('express').Router();
 const {
-  getStudents,
-  getSingleStudent,
-  createStudent,
-  deleteStudent,
-  addAssignment,
-  removeAssignment,
-} = require('../../controllers/studentController');
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController');
 
-// /api/students
-router.route('/').get(getStudents).post(createStudent);
+// /api/users
+router.route('/')
+  .get(getUser)//get all users
+  .post(createUser); // post new user
 
-// /api/students/:studentId
-router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
+// /api/users/:userId
+router.route('/:userId')
+  .get(getSingleUser)     // GET a single user by its _id
+  .put(updateUser)        // PUT to update a user by its _id
+  .delete(deleteUser);    // DELETE to remove user by its _id
 
-// /api/students/:studentId/assignments
-router.route('/:studentId/assignments').post(addAssignment);
-
-// /api/students/:studentId/assignments/:assignmentId
-router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)        // POST to add a friend to a user's friend list
+  .delete(removeFriend);  // DELETE to remove a friend from a user's friend list
 
 module.exports = router;
