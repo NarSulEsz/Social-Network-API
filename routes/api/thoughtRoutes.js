a6a2d25a4d27f26092db5ddc7fcfa7ec7b169b20
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-09-12 15:56:54 -0400

    Edited routes/api/thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,31 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtControllers');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/thoughts
+router.route('/')
+  .get(getThoughts) // GET all thoughts
+  .post(createThought); // POST a new thought
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+  .get(getSingleThought) // GET a single thought by its _id
+  .put(updateThought) // PUT to update a thought by its _id
+  .delete(deleteThought); // DELETE to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+  .post(addReaction); // POST to create a reaction
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction); // DELETE to remove a reaction
 
 module.exports = router;
